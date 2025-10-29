2e354ab366413db1d49240c02b7bb9b5f2e475dc
GABRIEL ALMEIDA <gabriel.almeida04@cs.unipe.edu.br>
2025-10-29 09:08:27 -0300

    exercicio4

---
+++ exercicio4/src/Duck.java
@@ -0,0 +1,17 @@
+public class Duck  {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+    public Duck() {
+    }
+    public void display() {};
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+}
